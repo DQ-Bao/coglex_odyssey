2ec29079cecb277f714d2a2af94f9bd71327657a
DQ-Bao <doanqbao2506@gmail.com>
2025-05-09 16:19:21 +0700

    move prompt to dialogue trigger

--- scripts/dialogue_trigger.gd
+++ scripts/dialogue_trigger.gd
@@ -2,6 +2,21 @@ extends Area2D
 
 @export var dialogue_resource: DialogueResource
 @export var dialogue_start: String = "Start"
+@onready var prompt = $Prompt
+@onready var area = $"."
+
+func _ready() -> void:
+	prompt.visible = false
+	area.connect("body_entered", _on_body_entered)
+	area.connect("body_exited", _on_body_exited)
+	
+func _on_body_entered(body):
+	if body.name == "player":
+		prompt.visible = true
+
+func _on_body_exited(body):
+	if body.name == "player":
+		prompt.visible = false
 
 func start() -> void:
 	DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start)
--- scripts/npc.gd
+++ scripts/npc.gd
@@ -1,28 +1,4 @@
 extends StaticBody2D
 
-var player_in_range = false
-var resource = load("res://dialogues/example.dialogue")
-var dialogue_playing = false
-
-@onready var prompt = $Prompt
-@onready var area = $DialogueTrigger
-
 func _ready() -> void:
 	$AnimatedSprite2D.play("Idle")
-	prompt.visible = false
-	area.connect("body_entered", _on_body_entered)
-	area.connect("body_exited", _on_body_exited)
-
-func _on_body_entered(body):
-	if body.name == "player":
-		player_in_range = true
-		prompt.visible = true
-
-func _on_body_exited(body):
-	if body.name == "player":
-		player_in_range = false
-		prompt.visible = false
-		
-func _process(_delta: float) -> void:
-	if player_in_range and Input.is_action_just_pressed("ui_accept"):
-		prompt.visible = false
