d3f187a83b967d2eb8f64cba0f951ee746dd397b
TienDat <multivncraft@gmail.com>
2025-05-09 15:33:09 +0700

    map update

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,7 +10,6 @@ var face_left = false
 func _physics_process(delta: float) -> void:
 	player_movement(delta)
 		
-	
 func player_movement(_delta):
 	var input_vec = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_vec * speed
