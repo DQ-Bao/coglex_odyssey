513e9b05d1e42b989bd9b1b17e5570815673c3a1
TienDat <multivncraft@gmail.com>
2025-05-09 20:51:23 +0700

    Auto stash before merge of "main" and "origin/main"

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -2,4 +2,5 @@ extends StaticBody2D
 @export var npc_animation : String = "Idle"
 
 func _ready() -> void:
-	$AnimatedSprite2D.play(npc_animation,0.3)
+	$AnimatedSprite2D.play(npc_animation)
+	#prompt.visible = false
